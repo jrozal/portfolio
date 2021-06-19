9caf95d4de8aaa2e125d2c5caf01f1b7c42dbf7b
jrozal <68182930+jrozal@users.noreply.github.com>
2021-06-18 22:03:09 -0700

    Add skills data

---
+++ src/data/skills.js
@@ -0,0 +1,22 @@
+export const skillsData = [
+  {
+    category: 'Front-End',
+    items: ['JavaScript', 'React', 'HTML', 'CSS', 'Styled-Components', 'jQuery', 'REST']
+  },
+  {
+    category: 'Back-End',
+    items: ['Node', 'Express', 'MySQL', 'PostgreSQL', 'MongoDB', 'Sequelize', 'Redis', 'Nginx', 'AWS EC2', 'AWS S3']
+  },
+  {
+    category: 'Development Tools',
+    items: ['Git', 'Bash', 'Webpack', 'NPM', 'Babel', 'Postman', 'Agile', 'TDD', 'CircleCI']
+  },
+  {
+    category: 'Testing',
+    items: ['Mocha', 'Chai', 'Jest', 'Artillery', 'Loader.io', 'New Relic']
+  },
+  {
+    category: 'Exploring',
+    items: ['C++', 'Python']
+  }
+];
