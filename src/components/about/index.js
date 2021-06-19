6ec2057247b508899b2703926985ae420c9ec16f
jrozal <68182930+jrozal@users.noreply.github.com>
2021-06-18 21:56:42 -0700

    Add about section

---
+++ src/components/about/index.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import styles from './about.module.scss';
+
+const About = () => (
+  <section className={styles.section}>
+    <div>
+      <h2>Hi, I'm Justin</h2>
+      <p>
+        A recent software engineering coding bootcamp graduate from the San Francisco Bay Area who enjoys using logic and reason to find elegant and effective solutions to complex problems. Motivated to learn how to leverage industry-leading frameworks and approach every day as an opportunity to extend my knowledge.
+      </p>
+    </div>
+  </section>
+);
+
+export default About;
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,13 +1,22 @@
 import React from 'react';
+import { Helmet } from 'react-helmet';
+import About from '../components/about';
 import Banner from '../components/banner';
 import Navigation from '../components/navigation';
 
 const IndexPage = () => {
   return (
-    <main>
-      <Banner/>
-      <Navigation/>
-    </main>
+    <div>
+      <Helmet>
+        <meta charSet="utf-8" />
+        <title>Justin Rozal</title>
+      </Helmet>
+      <main>
+        <Banner/>
+        <Navigation/>
+        <About/>
+      </main>
+    </div>
   );
 };
 
