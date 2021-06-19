07708866f03e9789fd0c951f5592550140b6764e
jrozal <68182930+jrozal@users.noreply.github.com>
2021-06-18 19:44:18 -0700

    Refactor sass variable names

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import Banner from '../components/banner';
+import Navigation from '../components/navigation';
 
 const IndexPage = () => {
   return (
     <main>
       <Banner/>
+      <Navigation/>
     </main>
   );
 };
