ff4cbcf2458469337b7caa0e9c32a3de8c1d2482
jrozal <68182930+jrozal@users.noreply.github.com>
2021-06-20 23:08:20 -0700

    Use responsive styling

--- src/components/about/index.js
+++ src/components/about/index.js
@@ -1,8 +1,8 @@
 import React from 'react';
-import styles from './about.module.scss';
+import './about.module.scss';
 
 const About = () => (
-  <section className={styles.section}>
+  <section className="component">
     <div>
       <h2>Hi, I'm Justin</h2>
       <p>
