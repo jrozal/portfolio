7af922ccdd503e62ed1ee683958c91f91ee3e1e3
jrozal <68182930+jrozal@users.noreply.github.com>
2021-06-21 19:31:14 -0700

    Add project data

---
+++ src/data/projects.js
@@ -0,0 +1,26 @@
+export const projectsData = [
+  {
+    title: 'Blitvazon',
+    description: 'An Amazon-like product features microservice designed with Service-Oriented Architecture.',
+    image: null,
+    stack: ['MongoDB', 'Express', 'React', 'Node']
+  }
+  {
+    title: 'Cloudbnb',
+    description: 'A scaled photos microservice for an existing Airbnb-like lodging and hospitality application',
+    image: null,
+    stack: ['Postgres', 'Sequelize', 'Express', 'React', 'Node']
+  }
+  {
+    title: 'Investfolio',
+    description: 'An investment portfolio dashboard and stock/crypto tracking application',
+    image: null,
+    stack: ['MySQL', 'Sequelize', 'Express', 'React', 'MaterialUI', 'Node']
+  }
+  {
+    title: 'Portfolio',
+    description: 'My personal portfolio used to showcase my experience and achievements.',
+    image: null,
+    stack: ['Gatsby', 'SASS', 'React', 'HTML']
+  }
+];
