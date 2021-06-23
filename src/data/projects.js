010c57d92f8c05ea67ddb7248b0d3a7a95674c96
jrozal <68182930+jrozal@users.noreply.github.com>
2021-06-22 21:20:04 -0700

    Update project data to import project images

--- src/data/projects.js
+++ src/data/projects.js
@@ -1,26 +1,30 @@
+import blitvazon from '../images/projects/blitvazon.jpg';
+import cloudbnb from '../images/projects/cloudbnb.jpg';
+import investfolio from '../images/projects/investfolio.jpg';
+
 export const projectsData = [
   {
     title: 'Blitvazon',
     description: 'An Amazon-like product features microservice designed with Service-Oriented Architecture.',
-    image: null,
+    image: blitvazon,
     stack: ['MongoDB', 'Express', 'React', 'Node']
-  }
+  },
   {
     title: 'Cloudbnb',
     description: 'A scaled photos microservice for an existing Airbnb-like lodging and hospitality application',
-    image: null,
+    image: cloudbnb,
     stack: ['Postgres', 'Sequelize', 'Express', 'React', 'Node']
-  }
+  },
   {
     title: 'Investfolio',
     description: 'An investment portfolio dashboard and stock/crypto tracking application',
-    image: null,
+    image: investfolio,
     stack: ['MySQL', 'Sequelize', 'Express', 'React', 'MaterialUI', 'Node']
-  }
+  },
   {
     title: 'Portfolio',
     description: 'My personal portfolio used to showcase my experience and achievements.',
     image: null,
-    stack: ['Gatsby', 'SASS', 'React', 'HTML']
+    stack: ['Gatsby', 'Sass', 'React', 'HTML']
   }
 ];
