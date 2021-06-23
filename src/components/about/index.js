3510b4b23cca36af860173f4fbd830bf183fe8f0
jrozal <68182930+jrozal@users.noreply.github.com>
2021-06-22 21:26:41 -0700

    Change about section p width

--- src/components/about/index.js
+++ src/components/about/index.js
@@ -1,9 +1,9 @@
 import React from 'react';
-import './about.module.scss';
+import styles from './about.module.scss';
 
 const About = () => (
   <section className="component">
-    <div>
+    <div className={styles.aboutWrapper}>
       <h2>Hi, I'm Justin</h2>
       <p>
         A recent software engineering coding bootcamp graduate from the San Francisco Bay Area who enjoys using logic and reason to find elegant and effective solutions to complex problems. Motivated to learn how to leverage industry-leading frameworks and approach every day as an opportunity to extend my knowledge.
