1a10b463e5f44b4210c3db6a2658f61f6f0759b6
jrozal <68182930+jrozal@users.noreply.github.com>
2021-06-23 00:03:31 -0700

    Add project component

--- src/components/projects/index.js
+++ src/components/projects/index.js
@@ -1,1 +1,22 @@
 import React from 'react';
+import { projectsData } from '../../data/projects';
+import ProjectCard from './project-card';
+import styles from './projects.module.scss';
+
+const Projects = () => (
+  <section className="component" id="projects">
+    <div>
+      <h2>Personal Projects</h2>
+    </div>
+    <div className={styles.projects}>
+      {projectsData.map((project, i) => (
+        <ProjectCard
+          key={i}
+          project={project}
+        />
+      ))}
+    </div>
+  </section>
+);
+
+export default Projects;
---
+++ src/components/projects/project-card/index.js
@@ -0,0 +1,57 @@
+import React, { useState } from 'react';
+import { isMobile } from 'react-device-detect';
+import styles from './project-card.module.scss';
+import SkillItem from '../../skills/skill-item';
+
+const ProjectCard = ({ project }) => {
+  const [flip, setFlip] = useState(false);
+
+  return (
+    <article className={styles.cardWrapper}>
+      <button
+        className={`${styles.card} ${flip ? styles.cardFlip : null}`}
+        onMouseEnter={() => setFlip(true)}
+        onMouseLeave={() => setFlip(false)}
+        onClick={() => setFlip(!flip)}
+      >
+        <div className={`${styles.cardSide} ${styles.cardFront}`}>
+          <img src={project.image} alt={project.title}></img>
+          <div className={styles.projectDescription}>
+            <h3>{project.title}</h3>
+            <p>{project.description}</p>
+          </div>
+          <span className={styles.hoverForMore}>
+            {isMobile ? 'Tap' : 'Hover'} for more
+          </span>
+        </div>
+        <div className={`${styles.cardSide} ${styles.cardBack}`}>
+          <div className={styles.techStackWrapper}>
+            <span className={styles.techStackTitle}>Tech Stack</span>
+            <ul className={styles.techStackList}>
+              {project.stack.map((tech, i) => (
+                <SkillItem key={i} skill={tech}/>
+              ))}
+            </ul>
+          </div>
+          <div className={styles.links}>
+            {project.links.map((link, i) => (
+              <a
+                key={i}
+                className={styles.button}
+                href={link.link}
+                target="_blank"
+                rel="noopener noreferrer"
+                aria-label=""
+              >
+                <span>{link.text}</span>
+                {link.icon}
+              </a>
+            ))}
+          </div>
+        </div>
+      </button>
+    </article>
+  )
+};
+
+export default ProjectCard;
